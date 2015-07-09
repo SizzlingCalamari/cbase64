a99feb89e906bf8b62df1a5201eab2af7a6e8e7a
Jordan Cristiano <jordan.cristi@gmail.com>
2015-07-09 19:28:30 -0400

    Added indicator for private functions

--- include/cbase64/cbase64.h
+++ include/cbase64/cbase64.h
@@ -62,6 +62,22 @@ int cbase64_encode_blockend(char* code_out, cbase64_encodestate* state_in);
 
 #ifdef CBASE64_IMPLEMENTATION
 
+char cbase64__encode_value(char value_in)
+{
+    static const char* encoding = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    if (value_in > 63) return '=';
+    return encoding[(int)value_in];
+}
+
+int cbase64__decode_value(char value_in)
+{
+    static const char decoding[] = {62,-1,-1,-1,63,52,53,54,55,56,57,58,59,60,61,-1,-1,-1,-2,-1,-1,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,-1,-1,-1,-1,-1,-1,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51};
+    static const char decoding_size = sizeof(decoding);
+    value_in -= 43;
+    if (value_in < 0 || value_in >= decoding_size) return -1;
+    return decoding[(int)value_in];
+}
+
 void cbase64_init_encodestate(cbase64_encodestate* state_in)
 {
     state_in->step = step_A;
@@ -90,22 +106,6 @@ unsigned int cbase64_calc_decoded_length(const char* code_in, unsigned int lengt
     return 3 * (length_in / 4) - (secondlast == '=') - (last == '=');
 }
 
-char cbase64_encode_value(char value_in)
-{
-    static const char* encoding = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    if (value_in > 63) return '=';
-    return encoding[(int)value_in];
-}
-
-int cbase64_decode_value(char value_in)
-{
-    static const char decoding[] = {62,-1,-1,-1,63,52,53,54,55,56,57,58,59,60,61,-1,-1,-1,-2,-1,-1,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,-1,-1,-1,-1,-1,-1,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51};
-    static const char decoding_size = sizeof(decoding);
-    value_in -= 43;
-    if (value_in < 0 || value_in >= decoding_size) return -1;
-    return decoding[(int)value_in];
-}
-
 int cbase64_encode_block(const char* plaintext_in, int length_in, char* code_out, cbase64_encodestate* state_in)
 {
     const char* plainchar = plaintext_in;
@@ -129,7 +129,7 @@ int cbase64_encode_block(const char* plaintext_in, int length_in, char* code_out
             }
             fragment = *plainchar++;
             result = (fragment & 0x0fc) >> 2;
-            *codechar++ = cbase64_encode_value(result);
+            *codechar++ = cbase64__encode_value(result);
             result = (fragment & 0x003) << 4;
     case step_B:
             if (plainchar == plaintextend)
@@ -140,7 +140,7 @@ int cbase64_encode_block(const char* plaintext_in, int length_in, char* code_out
             }
             fragment = *plainchar++;
             result |= (fragment & 0x0f0) >> 4;
-            *codechar++ = cbase64_encode_value(result);
+            *codechar++ = cbase64__encode_value(result);
             result = (fragment & 0x00f) << 2;
     case step_C:
             if (plainchar == plaintextend)
@@ -151,9 +151,9 @@ int cbase64_encode_block(const char* plaintext_in, int length_in, char* code_out
             }
             fragment = *plainchar++;
             result |= (fragment & 0x0c0) >> 6;
-            *codechar++ = cbase64_encode_value(result);
+            *codechar++ = cbase64__encode_value(result);
             result  = (fragment & 0x03f) >> 0;
-            *codechar++ = cbase64_encode_value(result);
+            *codechar++ = cbase64__encode_value(result);
         }
     }
     /* control should not reach here */
@@ -180,7 +180,7 @@ int cbase64_decode_block(const char* code_in, const int length_in, char* plainte
                     state_in->plainchar = *plainchar;
                     return plainchar - plaintext_out;
                 }
-                fragment = (char)cbase64_decode_value(*codechar++);
+                fragment = (char)cbase64__decode_value(*codechar++);
             } while (fragment < 0);
             *plainchar    = (fragment & 0x03f) << 2;
     case step_b:
@@ -191,7 +191,7 @@ int cbase64_decode_block(const char* code_in, const int length_in, char* plainte
                     state_in->plainchar = *plainchar;
                     return plainchar - plaintext_out;
                 }
-                fragment = (char)cbase64_decode_value(*codechar++);
+                fragment = (char)cbase64__decode_value(*codechar++);
             } while (fragment < 0);
             *plainchar++ |= (fragment & 0x030) >> 4;
             *plainchar    = (fragment & 0x00f) << 4;
@@ -203,7 +203,7 @@ int cbase64_decode_block(const char* code_in, const int length_in, char* plainte
                     state_in->plainchar = *plainchar;
                     return plainchar - plaintext_out;
                 }
-                fragment = (char)cbase64_decode_value(*codechar++);
+                fragment = (char)cbase64__decode_value(*codechar++);
             } while (fragment < 0);
             *plainchar++ |= (fragment & 0x03c) >> 2;
             *plainchar    = (fragment & 0x003) << 6;
@@ -215,7 +215,7 @@ int cbase64_decode_block(const char* code_in, const int length_in, char* plainte
                     state_in->plainchar = *plainchar;
                     return plainchar - plaintext_out;
                 }
-                fragment = (char)cbase64_decode_value(*codechar++);
+                fragment = (char)cbase64__decode_value(*codechar++);
             } while (fragment < 0);
             *plainchar++   |= (fragment & 0x03f);
         }
@@ -231,12 +231,12 @@ int cbase64_encode_blockend(char* code_out, cbase64_encodestate* state_in)
     switch (state_in->step)
     {
     case step_B:
-        *codechar++ = cbase64_encode_value(state_in->result);
+        *codechar++ = cbase64__encode_value(state_in->result);
         *codechar++ = '=';
         *codechar++ = '=';
         break;
     case step_C:
-        *codechar++ = cbase64_encode_value(state_in->result);
+        *codechar++ = cbase64__encode_value(state_in->result);
         *codechar++ = '=';
         break;
     case step_A:
